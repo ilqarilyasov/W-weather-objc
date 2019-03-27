5031b50dbfeb9a35614c87ea4eea72ecd29d0156
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-03-26 21:43:50 -0500

    Add a custom collection view cell class and implement updateViews method

--- WeatherObjC/WeatherObjC/Views/IIIWeatherCollectionViewCell.h
+++ WeatherObjC/WeatherObjC/Views/IIIWeatherCollectionViewCell.h
@@ -10,7 +10,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class IIIForecast;
 @interface IIIWeatherCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) IIIForecast *forecast;
 
 @end
 
--- WeatherObjC/WeatherObjC/Views/IIIWeatherCollectionViewCell.m
+++ WeatherObjC/WeatherObjC/Views/IIIWeatherCollectionViewCell.m
@@ -7,15 +7,34 @@
 //
 
 #import "IIIWeatherCollectionViewCell.h"
+#import "../Model/IIIForecast.h"
 
 @interface IIIWeatherCollectionViewCell()
 
 @property (weak, nonatomic) IBOutlet UIImageView *weatherImageView;
 @property (weak, nonatomic) IBOutlet UILabel *temperatureLabel;
 
-
 @end
 
 @implementation IIIWeatherCollectionViewCell
+
+- (void)updateViews
+{
+    if (self.forecast) {
+        NSNumber *tempDayNumber = [NSNumber numberWithDouble: self.forecast.tempDay];
+        NSString *tempDayString = [tempDayNumber stringValue];
+        [[self temperatureLabel] setText: tempDayString];
+        
+        NSString *firstIcon = [[[self forecast] icons] firstObject];
+        UIImage *icon = [UIImage imageNamed: firstIcon];
+        [[self weatherImageView] setImage: icon];
+    }
+}
+
+- (void)setForecast:(IIIForecast *)forecast
+{
+    _forecast = forecast;
+    [self updateViews];
+}
 
 @end
